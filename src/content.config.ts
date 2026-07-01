203ebab3486f2d9d0352145185228931d67ebd8f
Olsond <olsond.dy@gmail.com>
2026-07-01 18:01:47 +0800

    fix: remove external API loaders and unused pages to fix Netlify build timeout

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,6 @@
 import { glob, file } from 'astro/loaders'
 import { defineCollection } from 'astro:content'
 
-import { feedLoader } from '@ascorbic/feed-loader'
-import { githubReleasesLoader } from 'astro-loader-github-releases'
-import { githubPrsLoader } from 'astro-loader-github-prs'
-import { blueskyPostsLoader } from 'astro-loader-bluesky-posts'
-
 import {
   pageSchema,
   postSchema,
@@ -33,53 +28,6 @@ const projects = defineCollection({
   schema: projectSchema,
 })
 
-const releases = defineCollection({
-  loader: githubReleasesLoader({
-    mode: 'repoList',
-    repos: [
-      'withastro/astro',
-      'withastro/starlight',
-      'lin-stephanie/astro-loaders',
-      'lin-stephanie/astro-antfustyle-theme',
-    ],
-    monthsBack: 2,
-    entryReturnType: 'byRelease',
-    clearStore: true,
-  }),
-})
-
-const prs = defineCollection({
-  loader: githubPrsLoader({
-    search:
-      'repo:withastro/astro repo:withastro/starlight repo:lin-stephanie/astro-antfustyle-theme',
-    monthsBack: 1,
-    clearStore: true,
-  }),
-})
-
-const highlights = defineCollection({
-  loader: blueskyPostsLoader({
-    uris: [
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lifesehhok27',
-      'at://did:plc:iwhvwluesbbqtslwwdzgiize/app.bsky.feed.post/3lh3aonbqes2y',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lfwu3pka2c2j',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lfsayyhu4c2j',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lf3iyptedc2e',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lcv2yftszs2z',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lcl5ndm52c2s',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lcdimk36e226',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lbkb6hizhk2f',
-      'at://did:plc:oky5czdrnfjpqslsw2a5iclo/app.bsky.feed.post/3lbd2eaura22r',
-      'at://did:plc:oky5czdrnfjpqslsw2a5iclo/app.bsky.feed.post/3lbayyemhzs2v',
-      'at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.post/3larljiyi7s2v',
-    ],
-    newlineHandling: 'paragraph',
-    fetchThread: true,
-    threadDepth: 4,
-    fetchOnlyAuthorReplies: true,
-  }),
-})
-
 const photos = defineCollection({
   loader: file('src/content/photos/data.json'),
   schema: photoSchema,
@@ -103,23 +51,13 @@ const streams = defineCollection({
   schema: streamSchema,
 })
 
-const feeds = defineCollection({
-  loader: feedLoader({
-    url: 'https://astro.build/rss.xml',
-  }),
-})
-
 export const collections = {
   pages,
   home,
   blog,
   projects,
-  releases,
-  prs,
-  highlights,
   photos,
   shorts,
   changelog,
   streams,
-  feeds,
 }
